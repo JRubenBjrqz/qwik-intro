e9241c26bf61a7824b89a3eb8e5db866d8a18cc9
Rubén Bojórquez <107521558+JRubenBjrqz@users.noreply.github.com>
2023-05-14 23:34:50 -0700

    Functions and pokemon list

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,23 @@
-import { component$, useSignal } from '@builder.io/qwik';
+import { $, component$, useSignal } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
 
   const pokemonId = useSignal(1); // Primitive, booleans, strings
 
+  const changePokemonId = $(( value: number ) => {
+    if( ( pokemonId.value + value ) <= 0 ) return;
+    if( ( pokemonId.value + value ) >= 151 ) return;
+
+    pokemonId.value += value;
+  })
+  
+
   return (
     <>
       <header class="flex flex-col items-center justify-center">
-        <span class="text-2xl font-bold">Pokémon by Id</span>
-        <span class="text-3xl">{ pokemonId }</span>
+        <span class="text-3xl font-bold mb-2">Generation 1 </span>
+        <span class="text-1xl">{ pokemonId }</span>
 
         <img 
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${ pokemonId.value }.png`} 
@@ -17,8 +25,8 @@ export default component$(() => {
           style={{ width: '150px' }}/>
 
         <div>
-          <button onClick$={ () => pokemonId.value-- } class="btn btn-primary mr-2">Previous</button>
-          <button onClick$={  () => pokemonId.value++ } class="btn btn-primary">
+          <button onClick$={ () => changePokemonId(-1) } class="btn btn-primary mr-2">Previous</button>
+          <button onClick$={  () => changePokemonId(+1 ) } class="btn btn-primary">
             Next
           </button>
         </div>
