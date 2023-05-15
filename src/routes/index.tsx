740e36815fe5b4cf4d35d2167de3493e61f3bbe9
Rubén Bojórquez <107521558+JRubenBjrqz@users.noreply.github.com>
2023-05-14 22:44:58 -0700

    Signals and buttons

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,34 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
+
+  const pokemonId = useSignal(1); // Primitive, booleans, strings
+
   return (
     <>
       <header class="text-center">
-        <h1 class="text-7xl font-semibold">Hola Mundo</h1>
+        <span class="text-2xl">Searcher</span>
+        <span class="text-9xl block">{pokemonId}</span>
+
+        {/* To do: Pokemon image */}
+
+        <div>
+          <button class="btn btn-primary mr-2">Previous</button>
+          <button class="btn btn-primary">Next</button>
+        </div>
       </header>
+        
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: 'PokeQwik',
   meta: [
     {
       name: 'description',
-      content: 'Qwik site description',
+      content: 'Learning Qwik',
     },
   ],
 };
