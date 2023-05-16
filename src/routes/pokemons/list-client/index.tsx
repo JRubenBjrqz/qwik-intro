3947677ac45e5ad8c19390b30144d8cd938268fa
Rubén Bojórquez <107521558+JRubenBjrqz@users.noreply.github.com>
2023-05-15 22:54:38 -0700

    Qwik city link and titles

--- src/routes/pokemons/list-client/index.tsx
+++ src/routes/pokemons/list-client/index.tsx
@@ -1,6 +1,17 @@
 import { component$ } from '@builder.io/qwik';
+import { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
   
-    return <>Hello World - List client</>
+    return (
+        <>
+            <header class="flex flex-col items-center justify-center">
+                <span class="text-3xl font-bold mb-2">Client List</span>
+            </header>
+        </>
+    );
 });
+
+export const head: DocumentHead = {
+    title: 'Client List',
+};
--- src/routes/pokemons/list-ssr /index.tsx
+++ src/routes/pokemons/list-ssr /index.tsx
@@ -1,6 +1,17 @@
 import { component$ } from '@builder.io/qwik';
+import { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
   
-    return <>Hello World - List ssr</>
+    return (
+        <>
+            <header class="flex flex-col items-center justify-center">
+                <span class="text-3xl font-bold mb-2">SSR List</span>
+            </header>
+        </>
+    );
 });
+
+export const head: DocumentHead = {
+    title: 'SSR List',
+};
